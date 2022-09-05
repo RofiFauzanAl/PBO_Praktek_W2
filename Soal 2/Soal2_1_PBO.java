ac0abf8bf13b6c24e7abd9be5745aef2397555dd
Rofi Fauzan <91263023+RofiFauzanAl@users.noreply.github.com>
2022-09-05 21:39:13 +0700

    Add files via upload

---
+++ Soal 2/Soal2_1_PBO.java
@@ -0,0 +1,9 @@
+
+public class Soal2_1_PBO {
+	public static void main(String[] args) {
+		final double CM_PER_INCH = 2.54;
+		double paperWidth = 8.5;
+		double paperHeight = 11;
+		System.out.println("Paper size in centimeters : " + paperWidth * CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
+	}
+}
